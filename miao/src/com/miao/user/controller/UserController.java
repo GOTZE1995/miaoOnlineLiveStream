4b114bdc4dfcc9f4edb5a4413b38002c77e37cc1
a907471325 <907471325@qq.com>
2016-11-24 00:17:40 +0800

    更新结构

--- miao/src/com/miao/user/controller/UserController.java
+++ miao/src/com/miao/user/controller/UserController.java
@@ -53,9 +53,38 @@ public class UserController {
 		// 数据库中查到用户，将用户存到session域中
 		if (user != null) {
 			session.setAttribute("user", user);
+			//记录***登陆了
 			LogFactory.getLog(getClass()).info("用户" + user.getUserName() + "登录了");
 		}
 		return "index";
+	}
+	
+	/**
+	 * 检查登录时用户名与密码是否匹配
+	 * 
+	 * @param username
+	 * @param password
+	 * @param response
+	 * @author 孙兰云
+	 */
+	@RequestMapping("/loginCheckUserNameAndPwd")
+	public void loginCheckUserNameAndPwd(String username, String password, HttpServletResponse response) {
+		try {
+			String result = " not pass";
+			User user = userService.login(username, password);
+
+			// 数据库中查到用户，将用户存到session域中
+			if (user != null) {
+				result = "pass";
+			}
+
+			// 向页面返回数据
+			ServletOutputStream outputStream = response.getOutputStream();
+			outputStream.write(result.getBytes());
+			outputStream.close();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	/**
@@ -71,7 +100,7 @@ public class UserController {
 		HttpSession session = request.getSession();
 
 		if (session != null) {
-			session.removeAttribute("myName");
+			//移除session的用户信息
 			session.removeAttribute("user");
 		}
 		return "index";
@@ -88,6 +117,7 @@ public class UserController {
 	@RequestMapping("regist")
 	public String regist(User user, HttpSession session) {
 		userService.regist(user);
+		//注册成功直接登录
 		return login(user, session);
 	}
 
@@ -116,33 +146,7 @@ public class UserController {
 		}
 	}
 
-	/**
-	 * 检查登录时用户名与密码是否匹配
-	 * 
-	 * @param username
-	 * @param password
-	 * @param response
-	 * @author 孙兰云
-	 */
-	@RequestMapping("/loginCheckUserNameAndPwd")
-	public void loginCheckUserNameAndPwd(String username, String password, HttpServletResponse response) {
-		try {
-			String result = " not pass";
-			User u = userService.login(username, password);
-
-			// 数据库中查到用户，将用户存到session域中
-			if (u != null) {
-				result = "pass";
-			}
-
-			// 向页面返回数据
-			ServletOutputStream outputStream = response.getOutputStream();
-			outputStream.write(result.getBytes());
-			outputStream.close();
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+	
 
 	/**
 	 * 检测注册时邮箱是否重复
@@ -156,14 +160,14 @@ public class UserController {
 		try {
 			String result = "pass";
 
-			// 查询重复的用户名
+			// 查询重复的邮箱
 			List<User> users = userService.findAllUsersByEmail(email);
 
 			if (users != null && users.size() > 0) {
 				result = "repeat";
 			}
 
-			// 向页面返回数据
+			// 向页面返回结果
 			ServletOutputStream outputStream = response.getOutputStream();
 			outputStream.write(result.getBytes());
 			outputStream.close();
@@ -172,147 +176,7 @@ public class UserController {
 		}
 	}
 
-	/**
-	 * 后台用户列表界面+查询功能
-	 * 
-	 * @param request
-	 * @return jsp页面 2016/11/16
-	 */
-	@RequestMapping("/listUI")
-	public String listUI(@RequestParam(required = false, defaultValue = "1") Integer currentPage,
-			HttpServletRequest request, String searchName) {
-		Page<User> page;
-		List<User> list = null;
-		if (searchName != null && !"".equals(searchName)) {
-			// 根据搜索名查询
-			list = userService.doSearch(searchName);
-			// 将serchName设置成当前搜索的名称
-			request.setAttribute("searchName", searchName);
-		}
-		// 将list放到分页对象中，若没有数据，默认为第一页
-		page = userService.createPage(list, currentPage, 8);
-
-		request.setAttribute("page", page);
-		request.setAttribute("userList", userService.findAll());
-		return "WEB-INF/detail/listUser";
-	}
-
-	/**
-	 * 用户更新界面
-	 * 
-	 * @param id
-	 * @param request
-	 * @return jsp页面 2016/11/16
-	 */
-	@RequestMapping("/updateUI")
-	public String updateUI(Integer id, HttpServletRequest request) {
-		// 添加角色列表
-		request.setAttribute("roleList", roleService.findAll());
-		if (id != null) {
-			// 在request中设置要回显的数据
-			request.setAttribute("userInfo", userService.findById(id));
-		}
-		return "WEB-INF/detail/updateUser";
-	}
-
-	/**
-	 * 用户更新操作
-	 * 
-	 * @param user
-	 * @return jsp页面 2016/11/16
-	 */
-	@RequestMapping("/update")
-	public String update(User user, Integer roleId,
-			@RequestParam(name = "file", required = false) CommonsMultipartFile file, HttpServletRequest request) {
-		try {
-			if (user != null && user.getId() != null) {
-				if (roleId != null) {
-					// 根据id查询角色添加到用户
-					user.setRole(roleService.findById(roleId));
-				}
-				if (!file.isEmpty()) {
-					// 获得文件名并重命名
-					String fileName = UUID.randomUUID().toString() + file.getOriginalFilename();
-					// 获取完整路径
-					String filePath = request.getSession().getServletContext().getRealPath("/upload") + "\\" + fileName;
-					// 保存头像到upload目录
-					file.transferTo(new File(filePath));
-					// 将头像相对路径设置到user中
-					user.setHeadImg("upload/" + fileName);
-				}
-				userService.update(user);
-			}
-		} catch (IllegalStateException | IOException e) {
-			LogFactory.getLog(getClass()).equals("头像保存失败：" + e.getMessage());
-			throw new RuntimeException(e);
-		}
-		return "redirect:/user/listUI.do";
-	}
-
-	/**
-	 * 用户添加页面
-	 * 
-	 * @return jsp页面 2016/11/16
-	 */
-	@RequestMapping("/addUI")
-	public String addUI(HttpServletRequest request) {
-		// 添加角色列表
-		request.setAttribute("roleList", roleService.findAll());
-		return "WEB-INF/detail/saveUser";
-	}
-
-	/**
-	 * 用户添加操作
-	 * 
-	 * @param user
-	 * @return jsp页面 2016/11/16
-	 */
-	@RequestMapping(value = "/add", method = RequestMethod.POST)
-	public String add(User user, Integer roleId, @RequestParam(name = "file") CommonsMultipartFile file,
-			HttpServletRequest request) {
-		try {
-			if (user != null) {
-				if (roleId != null) {
-					// 根据id查询角色添加到用户
-					user.setRole(roleService.findById(roleId));
-				}
-				if (!file.isEmpty()) {
-					// 获得文件名并重命名
-					String fileName = UUID.randomUUID().toString() + file.getOriginalFilename();
-					// 获取完整路径
-					String filePath = request.getSession().getServletContext().getRealPath("/upload") + "\\" + fileName;
-					// 把文件保存到upload目录
-					file.transferTo(new File(filePath));
-					// 头像路径保存到user
-					user.setHeadImg("upload/" + fileName);
-				}
-				userService.save(user);
-			}
-		} catch (IllegalStateException | IOException e) {
-			LogFactory.getLog(getClass()).equals("头像保存失败：" + e.getMessage());
-			throw new RuntimeException(e);
-		}
-		return "redirect:/user/listUI.do";
-	}
-
-	/**
-	 * 用户删除操作
-	 * 
-	 * @param id
-	 * @return jsp页面 2016/11/16
-	 */
-	@RequestMapping("/delete")
-	public String delete(Integer id) {
-		try {
-			if (id != null) {
-				userService.deleteById(id);
-			}
-		} catch (Exception e) {
-			// 删除失败，事务自动回滚，跳转到list页面
-			return "redirect:/user/listUI.do";
-		}
-		return "redirect:/user/listUI.do";
-	}
+	
 
 	/**
 	 * 用户信息修改
@@ -393,8 +257,8 @@ public class UserController {
 	@RequestMapping("/checkNowPwd")
 	public void checkNowPwd(String nowPwd, HttpServletResponse response, HttpSession session) {
 		String result = "";
-		User u = (User) session.getAttribute("user");
-		String oldPwd = u.getPassword();
+		User user = (User) session.getAttribute("user");
+		String oldPwd = user.getPassword();
 		try {
 			if (nowPwd == "") {
 				result = "null";
@@ -410,6 +274,148 @@ public class UserController {
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
+	}
+	
+	/**
+	 * 后台用户列表界面+查询功能
+	 * 
+	 * @param request
+	 * @return jsp页面 2016/11/16
+	 */
+	@RequestMapping("/listUI")
+	public String listUI(@RequestParam(required = false, defaultValue = "1") Integer currentPage,
+			HttpServletRequest request, String searchName) {
+		Page<User> page;
+		List<User> list = null;
+		if (searchName != null && !"".equals(searchName)) {
+			// 根据搜索名查询
+			list = userService.doSearch(searchName);
+			// 将serchName设置成当前搜索的名称
+			request.setAttribute("searchName", searchName);
+		}
+		// 将list放到分页对象中，若没有数据，默认为第一页
+		page = userService.createPage(list, currentPage, 8);
+
+		request.setAttribute("page", page);
+		request.setAttribute("userList", userService.findAll());
+		return "WEB-INF/detail/listUser";
+	}
+
+	/**
+	 * 后台用户更新界面
+	 * 
+	 * @param id
+	 * @param request
+	 * @return jsp页面 2016/11/16
+	 */
+	@RequestMapping("/updateUI")
+	public String updateUI(Integer id, HttpServletRequest request) {
+		// 添加角色列表
+		request.setAttribute("roleList", roleService.findAll());
+		if (id != null) {
+			// 在request中设置要回显的数据
+			request.setAttribute("userInfo", userService.findById(id));
+		}
+		return "WEB-INF/detail/updateUser";
+	}
+
+	/**
+	 * 后台用户更新操作
+	 * 
+	 * @param user
+	 * @return jsp页面 2016/11/16
+	 */
+	@RequestMapping("/update")
+	public String update(User user, Integer roleId,
+			@RequestParam(name = "file", required = false) CommonsMultipartFile file, HttpServletRequest request) {
+		try {
+			if (user != null && user.getId() != null) {
+				if (roleId != null) {
+					// 根据id查询角色添加到用户
+					user.setRole(roleService.findById(roleId));
+				}
+				if (!file.isEmpty()) {
+					// 获得文件名并重命名
+					String fileName = UUID.randomUUID().toString() + file.getOriginalFilename();
+					// 获取完整路径
+					String filePath = request.getSession().getServletContext().getRealPath("/upload") + "\\" + fileName;
+					// 保存头像到upload目录
+					file.transferTo(new File(filePath));
+					// 将头像相对路径设置到user中
+					user.setHeadImg("upload/" + fileName);
+				}
+				userService.update(user);
+			}
+		} catch (IllegalStateException | IOException e) {
+			LogFactory.getLog(getClass()).equals("头像保存失败：" + e.getMessage());
+			throw new RuntimeException(e);
+		}
+		return "redirect:/user/listUI.do";
+	}
+
+	/**
+	 * 后台用户添加页面
+	 * 
+	 * @return jsp页面 2016/11/16
+	 */
+	@RequestMapping("/addUI")
+	public String addUI(HttpServletRequest request) {
+		// 添加角色列表
+		request.setAttribute("roleList", roleService.findAll());
+		return "WEB-INF/detail/saveUser";
+	}
+
+	/**
+	 * 后台用户添加操作
+	 * 
+	 * @param user
+	 * @return jsp页面 2016/11/16
+	 */
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String add(User user, Integer roleId, @RequestParam(name = "file") CommonsMultipartFile file,
+			HttpServletRequest request) {
+		try {
+			if (user != null) {
+				if (roleId != null) {
+					// 根据id查询角色添加到用户
+					user.setRole(roleService.findById(roleId));
+				}
+				if (!file.isEmpty()) {
+					// 获得文件名并重命名
+					String fileName = UUID.randomUUID().toString() + file.getOriginalFilename();
+					// 获取完整路径
+					String filePath = request.getSession().getServletContext().getRealPath("/upload") + "\\" + fileName;
+					// 把文件保存到upload目录
+					file.transferTo(new File(filePath));
+					// 头像路径保存到user
+					user.setHeadImg("upload/" + fileName);
+				}
+				userService.save(user);
+			}
+		} catch (IllegalStateException | IOException e) {
+			LogFactory.getLog(getClass()).equals("头像保存失败：" + e.getMessage());
+			throw new RuntimeException(e);
+		}
+		return "redirect:/user/listUI.do";
+	}
+
+	/**
+	 * 后台用户删除操作
+	 * 
+	 * @param id
+	 * @return jsp页面 2016/11/16
+	 */
+	@RequestMapping("/delete")
+	public String delete(Integer id) {
+		try {
+			if (id != null) {
+				userService.deleteById(id);
+			}
+		} catch (Exception e) {
+			// 删除失败，事务自动回滚，跳转到list页面
+			return "redirect:/user/listUI.do";
+		}
+		return "redirect:/user/listUI.do";
 	}
 
 }
