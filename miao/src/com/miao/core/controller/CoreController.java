25bce058f3dbb613deb41c3e68a1b3645e610ca6
a907471325 <907471325@qq.com>
2016-11-23 21:37:05 +0800

    后台登录controller

--- miao/src/com/miao/core/controller/CoreController.java
+++ miao/src/com/miao/core/controller/CoreController.java
@@ -1,42 +1,59 @@
 package com.miao.core.controller;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-/**
- * 后台核心控制器
- * @author Jupiter
- *
- */
+
+import com.miao.entity.User;
+import com.miao.user.service.UserService;
+
 @Controller
 @RequestMapping("sys")
 public class CoreController {
+	
+	@Resource
+	private UserService userService;
 
-	/**
-	 * 跳转到首页
-	 * @return
-	 */
 	@RequestMapping("/index")
-	public String index() {
+	public String index(){
 		return "WEB-INF/index";
 	}
-
 	@RequestMapping("left")
-	public String leftUI() {
+	public String leftUI(){
 		return "WEB-INF/detail/left";
 	}
-
 	@RequestMapping("top")
-	public String topUI() {
+	public String topUI(){
 		return "WEB-INF/detail/top";
 	}
-
 	@RequestMapping("bottom")
-	public String bottomUI() {
+	public String bottomUI(){
 		return "WEB-INF/detail/bottom";
 	}
-
-	@RequestMapping("right")
-	public String rightUI() {
-		return "WEB-INF/detail/right";
+	
+	@RequestMapping("login_back")
+	public String login_back(User user,HttpSession session){
+        user = userService.login(user.getUserName(),user.getPassword());
+		
+		//数据库中查到用户，将用户存到session域中
+		if(user!=null){
+			session.setAttribute("user", user);
+			return "WEB-INF/detail/right";
+		}else{
+			return "WEB-INF/detail/login_back";
+		}
+	}
+	
+	@RequestMapping("logout_back")
+	public String logout_back(HttpServletRequest request){
+		HttpSession session = request.getSession();
+		
+		if(session!=null){
+			session.removeAttribute("user");
+		}
+		return "redirect:/sys/login_back.do";
 	}
 }
