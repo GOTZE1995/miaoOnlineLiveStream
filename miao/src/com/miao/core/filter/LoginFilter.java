2e3343a420dd410b8dda41d0042f5d48911144fe
a907471325 <907471325@qq.com>
2016-11-28 13:33:05 +0800

    添加过滤器

---
+++ miao/src/com/miao/core/filter/LoginFilter.java
@@ -0,0 +1,61 @@
+package com.miao.core.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * 后台登录过滤器
+ * @author 程菊飞
+ * 2016/11/26
+ */
+public class LoginFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest serveletRequest, ServletResponse servletResponse, FilterChain chain)
+			throws IOException, ServletException {
+		System.out.println("进入到过滤器了。");
+		HttpServletRequest request=(HttpServletRequest)serveletRequest;
+		HttpServletResponse response=(HttpServletResponse)servletResponse;
+		HttpSession session=request.getSession();
+		String uri=request.getRequestURI();
+		//判断当前请求是否是登陆的请求地址
+		if(!uri.contains("login_back")){
+			//不是后台的登录请求
+			if(uri.contains("UI")||uri.contains("delete")){
+				//是后台的请求
+				if(session.getAttribute("AdminUser")!=null){
+					chain.doFilter(request, response);
+					//已经登录，让其继续执行
+				}else{
+					//没有登陆，跳转到后台登录页面
+					response.sendRedirect(request.getContextPath()+"/sys/index.do");
+				}
+			}else{
+				//不是关于后台的请求
+				chain.doFilter(request, response);
+			}
+		}else{
+			//是后台的登陆请求
+			chain.doFilter(request, response);
+		}
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		
+	}
+	
+	@Override
+	public void destroy() {
+
+	}
+}
---
+++ miao/src/com/miao/core/filter/PreLoginFilter.java
@@ -0,0 +1,57 @@
+package com.miao.core.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 前台登录过滤器
+ * @author 孙兰云
+ * 2016/11/27
+ */
+public class PreLoginFilter implements Filter {
+
+    public PreLoginFilter() {
+
+    }
+
+	public void destroy() {
+
+	}
+
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+
+		HttpServletRequest request = (HttpServletRequest)req;
+		HttpServletResponse response = (HttpServletResponse)resp;
+		
+		String uri = request.getRequestURI();
+		
+		//过滤非登录方法
+		if (!uri.contains("login")) {
+			Object object = request.getSession().getAttribute("user");
+			
+			//已登录，放行
+			if (object != null){
+				chain.doFilter(request, response);
+			}
+			//未登录，跳转到登录页面
+			else{ 
+				response.sendRedirect(request.getContextPath()+"/index.jsp");
+			}
+		}   
+		//登录方法，放行
+		else{
+			chain.doFilter(request, response);
+		}
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+
+	}
+}
