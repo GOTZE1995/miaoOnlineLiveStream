c84af83d5153737fba29325f290f8bddbbc96d62
sly1995 <1562110092@qq.com>
2016-12-14 20:22:43 +0800

    完善聊天室代码

--- miao/src/com/miao/chat/controller/ChatServer.java
+++ miao/src/com/miao/chat/controller/ChatServer.java
@@ -6,7 +6,6 @@ import java.util.Date;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 import javax.websocket.OnClose;
-import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
@@ -22,11 +21,8 @@ import net.sf.json.JSONObject;
 @ServerEndpoint("/chatroom")  //编程式注解
 public class ChatServer {
 
-	//设置消息发送日期的显示格式
+	// 设置消息发送日期的显示格式
 	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("HH:mm:ss");
-	
-	// 静态变量，记录在线人数。
-	public static int onlineCount = 0;
 
 	// concurrent包的线程安全Set，用来存放每个客户端对应的ChatServer对象。
 	private static CopyOnWriteArraySet<ChatServer> webSocketSet = new CopyOnWriteArraySet<ChatServer>();
@@ -44,12 +40,16 @@ public class ChatServer {
 		
 		// 加入set中
 		webSocketSet.add(this); 
+	}
+	
+	/**
+	 * 关闭一个会话
+	 */
+	@OnClose
+	public void onClose() {
 		
-		 // 在线数加1
-		addOnlineCount();
-		
-		
-		System.out.println("有新连接加入！当前在线人数为" + getOnlineCount());
+		// 从set中删除
+		webSocketSet.remove(this); 
 	}
 
 	@OnMessage
@@ -63,12 +63,6 @@ public class ChatServer {
 				// 在消息中添加发送时间
 				jsonObj.put("date", DATE_FORMAT.format(new Date()));
 
-				// 遍历所有的消息
-				for (Session openSession : session.getOpenSessions()) {
-
-					// 在消息中添加是否是当前会话的标志
-					jsonObj.put("isSelf", openSession.equals(session));	
-				}
 				// 发送json格式的消息
 				item.sendMessage(jsonObj.toString());
 			} catch (IOException e) {
@@ -79,58 +73,11 @@ public class ChatServer {
 	}
 
 	/**
-	 * 关闭一个会话
-	 */
-	@OnClose
-	public void onClose() {
-		
-		// 从set中删除
-		webSocketSet.remove(this); 
-		
-		// 在线数减1
-		subOnlineCount(); 
-		
-		System.out.println("有一连接关闭！当前在线人数为" + getOnlineCount());
-	}
-
-	/**
-	 *  错误处理
-	 */
-	@OnError
-	public void onError(Throwable t) {
-		System.out.println("发生错误");
-		
-		t.printStackTrace();
-	}
-
-	/**
 	 * 发送消息
 	 * @param message
 	 * @throws IOException
 	 */
 	public void sendMessage(String message) throws IOException {
 		this.session.getBasicRemote().sendText(message);
-	}
-
-	/**
-	 * 获取在线人数
-	 * @return
-	 */
-	public static synchronized int getOnlineCount() {
-		return onlineCount;
-	}
-
-	/**
-	 * 在线人数加1
-	 */
-	public static synchronized void addOnlineCount() {
-		ChatServer.onlineCount++;
-	}
-
-	/**
-	 * 在线人数减1
-	 */
-	public static synchronized void subOnlineCount() {
-		ChatServer.onlineCount--;
 	}
 }
